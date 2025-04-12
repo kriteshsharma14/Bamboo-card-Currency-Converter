36a8ae992664422a4671f0c8f3ff72d6dc945913
Kritesh Sharma <kriteshsharma14@gmail.com>
2025-04-12 15:24:46 +0530

    add currency repositiry to call http api for conversion

---
+++ Bamboo-card-currency-convertor/Bamboo-card-currency-convertor/Repositories/Interface/ICurrencyRepository.cs
@@ -0,0 +1,9 @@
+﻿using Bamboo_card_currency_convertor.Models;
+
+namespace Bamboo_card_currency_convertor.Repositories.Interface
+{
+    public interface ICurrencyRepository
+    {
+        Task<CurrencyConversionResponse> GetLatestConversion(string from, List<string> to);
+    }
+}
